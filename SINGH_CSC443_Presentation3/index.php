762d0cbf80aa5b93993d353f99f44b12ceca6e93
Jass-Singh-20 <81336464+Jass-Singh-20@users.noreply.github.com>
2022-11-16 15:34:24 -0500

    Add files via upload

---
+++ SINGH_CSC443_Presentation3/index.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <link rel="stylesheet" href="css/webslides.css">
+        <title>Jasdip Singh</title>
+
+    </head>
+    <body>
+        <main role="main"></main>
+        <article id="webslides" class="horizontal">
+             <!-- Slide 1 -->
+            <section>
+                <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+           <center>     <div class="wrap">
+                    
+                    <h1><p style="color:white;">Jasdip Singh</p>                       
+                    </h1>
+                    <br><br>
+                    <h2 ><p style="color:white;">Presentation 3</p></h2>
+                    <br><br>
+                    <h2 ><p style="color:white;">Southern Connecticut State University </p></h2>
+                    <img src="img/mypic.jpg" alt="" style="width:300px;height:250px;">
+                </div> </center>
+                </section>
+                <!--slide2-->
+        <section> 
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Discuss the peer-review feedback you received for Assignment 4</p></h3>
+                        <br><br>
+                        <p class ="text-intro" style="color:white;">
+                            I would like to Thank everyone for taking the time to review and comment upon our Theme park.I found the advice constructive and have incorporated many of the suggestions into my revision.
+                            I've responded to each response individually and would like use these suggestions and peer reviews to improve my skill sets and elaborate more on some of the content. Thank you again for your thoughtful comments…
+                        </p>
+
+                       
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                <center><img src ="https://cryocooler.org/resources/Pictures/ICC21Conf/PAPERRVW.GIF" alt ="" style="width:400px;height:300px; border-radius:20px;"  ></center>
+            </div>
+            
+            
+            
+        </section>     
+        
+        <!--slide 3-->
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Response to peer review - Research</p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            Research - I really like couple of the feedbacks about adding more php in my code. After getting this feedback I actually started looking
+                            into how to do simple process in just php. As I looked further into this topic I figured that there are actually major advantanges of 
+                            using php. php basically give the ability to engage with open source software. Its more compatible with HTML and provides Cheaper hosting services
+                            and Good flexibility. 
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <!--slide 4-->
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Display your solution to your Research</p></h3>
+                    <br><br><br><br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/lab1.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Response to Peer Review  - Implementation and acknowledgement </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            After getting that feedback I decided to put some time in and tried implmenting php to display the the .csv file from the 
+                            assignment that we did before. So the code in the previous slide is making a table, opening up a .csv and appeding the peer review the data into
+                            those tables. This implentation was a little hard but I think it was worth the time as I learned how to open files and read files in php.
+                             
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Response to peer review - Research & acknowledgement</p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            Another great feedback I recieved was about having spelling errors in my presentation. This feedback was much important. I usually pay less
+                            attention to the spellings because I'm always more focused on designing and backend. but now
+                            I actually pay attention to what I'm wrting and always double check my content. 
+                            <br> <br>
+                        </p>
+                        
+                        
+                        
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Navigate to your themepark area through the CSC 443 Themepark portal</p></h3>
+                    <br><br><br><br><br>
+                    <a href="http://167.99.253.247/csc443/themepark/" target="_blank" style="color: white">Theme park portal</a>
+                    <br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/lab_2.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Discuss your plans or questions you have for each of the following: </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                          HTML - Layout Structure.
+                          Sometime I'll have to use HTML to create layouts that a customer has given to me. It's a very challenging Task sometime.
+                          So my main focus going forward is improving the structure my HTML. HTML layout also impact on how you write your CSS and javascript.
+                          
+                          
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">CSS </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                          CSS - Designing
+                          My plans going forward with CSS is just improving my desiging skills with links, buttons and hrefs. Also focusing more on
+                          background gradients.
+                          
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Javascript </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                          Javascript - It think there is much more to learn with javascript yet. Even though I'm very much 
+                          familliar with how to use js from server and client side. With js I would like to mostly work on how to use js to asynchronously
+                          display data. This is a very important aspect of JavaScript, Either you are fetching data from the backend or you are processing requests asynchronously in the backend itself.
+                        
+                          
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">PHP </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            With PHP I would like to mostly focus on improving my skills with Sessions and Cookies. I've been practising and playing around
+                            with sessions and cookies but not there yet. I think I need to spend more time on this topic to be professionally good at it.
+                          
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">client-side interactive content </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            I would like to focus more on displaying content with color cordination on client side. I think that's the most 
+                            important part displaying right colors according to the content that you delivering to the audience. 
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">client-side dynamic content </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            I would like to focus more on how I display or deliver content to the user on client side dynamically. Also going to work more on displaying
+                            more content on the client side for faster and flexible experience for the user.
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">server-side dynamic content </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                           With server side dynamic content I would like to learn more about Modules. I think modules play an imporant role 
+                           on doing something in a much easier way instead of writing a code from scratch. We can also use bunch of modules
+                           on service side to display content with REST API calls.
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">server-side interactive content </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                           With server-side interactive content I would like to jump more into using AJAX and asynchronous functions to display data.
+                           I think this is very important as the data will be displayed asynchronously resulting in a more flexibility and time saving.
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">data-driven dynamic content </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                         Focusing more on using php and js with sql databases. Also working on how information of each user that visits and interacts with the pages is registered in 
+                         those databses. 
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+        
+        <section>
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+           <center>     <div class="wrap">
+                    
+                    <h3><p style="color:white;">Thank you everyone and everyone is welcome to share feedback (questions & suggestions) through my team 3 on BB</p>                       
+                    </h3>
+                    
+                    
+                    
+                    <img src="img/owl.png" alt="" style="width:500px;height:200px;"> <br><br>
+                    <input type="button" class="button" onclick="location.href='index.html#slide=1'" style="width:auto; border-radius:20px;" value="Begin">
+                </div> </center>
+                </section>
+        </section>
+        <script src="js/webslides.js"></script>  
+        <script>
+            window.ws = new WebSlides();
+        </script> 
+    </body>
+</html>
